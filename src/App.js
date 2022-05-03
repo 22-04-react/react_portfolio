115de2cd89557b09960e6a309832a01faca5352d
tubi55 <tubi55@nate.com>
2022-05-03 17:50:41 +0900

    기본 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
+import { Route } from 'react-router-dom';
+
+//common
+import Header from './components/common/Header';
+import Footer from './components/common/Footer';
+
+//main
+import Visual from './components/main/Visual';
+import Content from './components/main/Content';
+
+//sub
+import Department from './components/sub/Department';
+import Gallery from './components/sub/Gallery';
+
 function App() {
 	return (
-		<h1>Hello</h1>
+		<>
+			<Header />
+
+			<Route exact path='/'>
+				<Visual />
+				<Content />
+			</Route>
+
+			<Route path='/department'>
+				<Department />
+			</Route>
+
+			<Route path='/gallery'>
+				<Gallery />
+			</Route>
+
+			<Footer />
+		</>
 	);
 }
 
