950da20031f960bdb2f718db1f8713983c6fc395
tubi55 <tubi55@nate.com>
2022-05-04 11:04:08 +0900

    activeStyle 메뉴 활성화

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,34 +1,47 @@
-import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 function Header() {
+	const active = { color: 'aqua' };
+
 	return (
 		<header>
 			<div className='inner'>
 				<h1>
-					<NavLink exact to='/'>
+					<NavLink activeStyle={active} exact to='/'>
 						LOGO
 					</NavLink>
 				</h1>
 
 				<ul id='gnb'>
 					<li>
-						<NavLink to='/department'>Department</NavLink>
+						<NavLink activeStyle={active} to='/department'>
+							Department
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to='/community'>Community</NavLink>
+						<NavLink activeStyle={active} to='/community'>
+							Community
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to='/gallery'>Gallery</NavLink>
+						<NavLink activeStyle={active} to='/gallery'>
+							Gallery
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to='/youtube'>Youtube</NavLink>
+						<NavLink activeStyle={active} to='/youtube'>
+							Youtube
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to='/location'>Location</NavLink>
+						<NavLink activeStyle={active} to='/location'>
+							Location
+						</NavLink>
 					</li>
 					<li>
-						<NavLink to='/join'>Join</NavLink>
+						<NavLink activeStyle={active} to='/join'>
+							Join
+						</NavLink>
 					</li>
 				</ul>
 			</div>
