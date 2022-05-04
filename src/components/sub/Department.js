a21b744d5303fcfecc87ca156cdc4e2ffe767483
tubi55 <tubi55@nate.com>
2022-05-04 14:22:35 +0900

    useEffect를 통한 컴포넌트의 생명주기 활용

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,9 +1,30 @@
 import Layout from '../common/Layout';
+import { useEffect, useState } from 'react';
 
 function Department() {
+	let [num, setNum] = useState(0);
+
+	//현재 useEffect의 의존성 배열은 비어있으므로
+	useEffect(() => {
+		//컴포넌트가 생성시 한번만 실행
+		console.log('department생성');
+
+		//컴포넌트 소멸시 한번만 실행
+		return () => {
+			console.log('department소멸');
+		};
+	}, []);
+
+	//useEffect의 의존성 배열이 num으로 지정되어 있으므로
+	useEffect(() => {
+		//num값이 변경될때마다 실행
+		console.log(num);
+	}, [num]);
+
 	return (
 		<Layout name={'Department'}>
-			<p>디파트먼트 컨텐츠가 들어올자리</p>
+			<button onClick={() => setNum(num + 1)}>증가</button>
+			<h1>{num}</h1>
 		</Layout>
 	);
 }
