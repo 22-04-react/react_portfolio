553472276394ada3611b9a3e0edaeb85e35bbd4e
tubi55 <tubi55@nate.com>
2022-05-04 09:49:01 +0900

    기본 라우터 연결

--- src/App.js
+++ src/App.js
@@ -10,7 +10,11 @@ import Content from './components/main/Content';
 
 //sub
 import Department from './components/sub/Department';
+import Community from './components/sub/Community';
 import Gallery from './components/sub/Gallery';
+import Youtube from './components/sub/Youtube';
+import Location from './components/sub/Location';
+import Join from './components/sub/Join';
 
 function App() {
 	return (
@@ -26,8 +30,24 @@ function App() {
 				<Department />
 			</Route>
 
+			<Route path='/community'>
+				<Community />
+			</Route>
+
 			<Route path='/gallery'>
 				<Gallery />
+			</Route>
+
+			<Route path='/youtube'>
+				<Youtube />
+			</Route>
+
+			<Route path='/location'>
+				<Location />
+			</Route>
+
+			<Route path='/join'>
+				<Join />
 			</Route>
 
 			<Footer />
@@ -36,3 +56,16 @@ function App() {
 }
 
 export default App;
+
+/*
+	SSR vs CSR
+	SSR (Server Side Rendering)
+	- 페이지 이동할때마다 일일이 서버쯕에 출력될 html파일을 요청
+	- 장점 - 초기 로딩속도가 빠름, 검색최적화 SEO
+	- 단점 - 페이지 이동시, 같은 페이지안에서 컨텐츠가 동적으로 바꿔야될때 사용성 낮음
+
+	CSR (Client Side Rendering)
+	- 초기에 화면에 출력될 모든 정보 데이터를 chunk단위로 모두 불러옴
+	- 장점 - 같은 페이지안에서 서로 다른 컨텐츠를 실시간으로 변경하면서 출력하므로 사용성이 좋음
+	- 단점 - 초기 로딩속도가 느림, 검색엔진 비최적화
+*/
---
+++ src/components/sub/Location.js
@@ -0,0 +1,12 @@
+//rfce (React Functional Component Export)
+import React from 'react';
+
+function Locaition() {
+	return (
+		<div>
+			<h1>Location</h1>
+		</div>
+	);
+}
+
+export default Locaition;
