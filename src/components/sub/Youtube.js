a9691a2694df81df774b0ce2a4897b4e736bb09e
tubi55 <tubi55@nate.com>
2022-05-06 10:50:20 +0900

    유튜브 데이터 바인딩

--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -1,9 +1,35 @@
 import Layout from '../common/Layout';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Youtube() {
+	const [vids, setVids] = useState([]);
+	useEffect(() => {
+		const key = 'AIzaSyBZFBuapkASPcRBXB2-d_ak5-ecCpVicI4';
+		const playlistId = 'PLHtvRFLN5v-UVVpNfWqtgZ6YPs9ZJMWRK';
+		const num = 5;
+		const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playlistId}&maxResults=${num}`;
+
+		axios.get(url).then((json) => {
+			console.log(json.data.items);
+			setVids(json.data.items);
+		});
+	}, []);
+
 	return (
 		<Layout name={'Youtube'}>
-			<p>유튜브 컨텐츠가 들어올자리</p>
+			{vids.map((vid, idx) => {
+				return (
+					<article key={idx}>
+						<div className='pic'>
+							<img src={vid.snippet.thumbnails.standard.url} />
+							<h2>{vid.snippet.title}</h2>
+							<p>{vid.snippet.description}</p>
+							<span>{vid.snippet.publishedAt}</span>
+						</div>
+					</article>
+				);
+			})}
 		</Layout>
 	);
 }
