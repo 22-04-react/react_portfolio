4a6a2521595e025c617276fad1964909ba83f4d1
tubi55 <tubi55@nate.com>
2022-05-06 10:23:05 +0900

    멤버정보 변경, state의 불변성 유지

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -4,18 +4,27 @@ import axios from 'axios';
 const path = process.env.PUBLIC_URL;
 
 function Department() {
+	console.log('department render');
 	const [members, setMembers] = useState([]);
 
 	useEffect(() => {
 		axios.get(`${path}/DB/member.json`).then((json) => {
 			console.log(json.data.members);
 			setMembers(json.data.members);
-			console.log(members);
 		});
 	}, []);
 
 	return (
 		<Layout name={'Department'}>
+			<button
+				onClick={() => {
+					const newMembers = [...members];
+					newMembers[0].name = 'Emma';
+					setMembers(newMembers);
+				}}>
+				정보변경
+			</button>
+
 			<ul className='memberList'>
 				{members.map((member, idx) => {
 					return (
