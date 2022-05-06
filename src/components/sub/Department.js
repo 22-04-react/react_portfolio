2879005d4cda8dedeb264af9845f9e76a8df109d
tubi55 <tubi55@nate.com>
2022-05-06 09:48:53 +0900

    axios로 받아온 데이터 state에 담고 반복출력

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,17 +1,32 @@
 import Layout from '../common/Layout';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
-
 const path = process.env.PUBLIC_URL;
 
 function Department() {
+	const [members, setMembers] = useState([]);
+
 	useEffect(() => {
-		axios
-			.get(`${path}/DB/member.json`)
-			.then((json) => console.log(json.data.members));
+		axios.get(`${path}/DB/member.json`).then((json) => {
+			console.log(json.data.members);
+			setMembers(json.data.members);
+			console.log(members);
+		});
 	}, []);
 
-	return <Layout name={'Department'}></Layout>;
+	return (
+		<Layout name={'Department'}>
+			<ul className='memberList'>
+				{members.map((member, idx) => {
+					return (
+						<li key={idx}>
+							<h2>{member.name}</h2>
+						</li>
+					);
+				})}
+			</ul>
+		</Layout>
+	);
 }
 
 export default Department;
