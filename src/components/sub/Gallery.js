605c611e75aec7153776283ddc0b42087feead4a
tubi55 <tubi55@nate.com>
2022-05-06 15:40:37 +0900

    flickr 리스트 스타일링

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -8,7 +8,7 @@ function Gallery() {
 	useEffect(() => {
 		const key = '4612601b324a2fe5a1f5f7402bf8d87a';
 		const method_interest = 'flickr.interestingness.getList';
-		const num = 5;
+		const num = 20;
 		const url = `https://www.flickr.com/services/rest/?method=${method_interest}&per_page=${num}&api_key=${key}&nojsoncallback=1&format=json`;
 		axios.get(url).then((json) => {
 			console.log(json.data.photos.photo);
