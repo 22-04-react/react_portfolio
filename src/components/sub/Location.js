06e40c4df4b75dc7a05e49774ce17c9d9b39d06b
tubi55 <tubi55@nate.com>
2022-05-09 10:39:55 +0900

    useEffect로 state값 변경 사이틀 주석 설명

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -13,11 +13,15 @@ function Locaition() {
 	const [map, setMap] = useState(null);
 	const [traffic, setTraffic] = useState(false);
 
+	//해당 컴포넌트가 처음 로딩될떄 한번만 실행
 	useEffect(() => {
 		const map_instance = new kakao.maps.Map(container.current, options);
+		//카카오 생성자로 만들어진 인스턴스를 map스테이트에 옮겨담아 해당 컴포넌트 전역에서 map_instance를 활용
+		//useEffect에 의해서 state에 특정 정보값을 변경하면 다음번 싸이클에서 변경된 정보값을 활용가능
 		setMap(map_instance);
 	}, []);
 
+	//해당 컴포넌트에서 traffic정보값이 변경될때마다 실행
 	useEffect(() => {
 		// 처음 mount시 map값이 비어있어서 오류나는걸 해결하기 위해
 		// map값이 담겨있을때에만 구문실행되도록 조건문 설정
