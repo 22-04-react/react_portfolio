46d08b0c1659137d7f3b483dcc98dab6c7e332ed
tubi55 <tubi55@nate.com>
2022-05-09 14:33:17 +0900

    인풋요소에 값 입력시 값이 전달되는 순서

--- src/components/sub/Join.js
+++ src/components/sub/Join.js
@@ -9,10 +9,11 @@ function Join() {
 	const [val, setVal] = useState(initVal);
 
 	const handleChange = (e) => {
+		//순서2 - 입력하고 있는 input요소의 name, value값을 변수로 비구조할당
 		const { name, value } = e.target;
-		console.log('name', name);
-		console.log('value', value);
-		//setVal({...val, 'userid': value})
+
+		//setVal({ ...val, userid: value });
+		//순서3 - 기존 val값을 복사해서 방급 입력받은 value값으로 덮어쓰기
 		setVal({ ...val, [name]: value });
 	};
 
@@ -34,7 +35,9 @@ function Join() {
 										id='userid'
 										name='userid'
 										placeholder='아이디를 입력하세요'
+										//val스테이트의 userid값이 인풋요소에 출력
 										value={val.userid}
+										//순서1- 인풋에 값을 입력하면 handleChange함수 호출
 										onChange={handleChange}
 									/>
 								</td>
