b55d775112402e1136bdf1ce64dd2f651ca621c0
tubi55 <tubi55@nate.com>
2022-05-09 14:20:48 +0900

    input요소에 값을 입력하면서 onChange이벤트 발생시 입력한 값을 val state에 저장하고 저장된 값이 다시 value로  input요소에 출력

--- src/components/sub/Join.js
+++ src/components/sub/Join.js
@@ -1,9 +1,54 @@
 import Layout from '../common/Layout';
+import { useState } from 'react';
 
 function Join() {
+	const initVal = {
+		userid: '',
+	};
+
+	const [val, setVal] = useState(initVal);
+
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		console.log('name', name);
+		console.log('value', value);
+		//setVal({...val, 'userid': value})
+		setVal({ ...val, [name]: value });
+	};
+
 	return (
 		<Layout name={'Join'}>
-			<p>조인 컨텐츠가 들어올자리</p>
+			<form>
+				<fieldset>
+					<legend>회원가입 폼 양식</legend>
+					<table border='1'>
+						<caption>회원가입 정보입력</caption>
+						<tbody>
+							<tr>
+								<th scope='row'>
+									<label htmlFor='userid'>USER ID</label>
+								</th>
+								<td>
+									<input
+										type='text'
+										id='userid'
+										name='userid'
+										placeholder='아이디를 입력하세요'
+										value={val.userid}
+										onChange={handleChange}
+									/>
+								</td>
+							</tr>
+							<tr>
+								<th colSpan='2'>
+									<input type='reset' value='CANCEL' />
+									<input type='submit' value='SEND' />
+								</th>
+							</tr>
+						</tbody>
+					</table>
+				</fieldset>
+			</form>
 		</Layout>
 	);
 }
