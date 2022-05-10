5fc85df67abcc85611cc911ace2d716a3b8b889f
tubi55 <tubi55@nate.com>
2022-05-10 09:15:25 +0900

    기존 crud 폼 생성

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -1,9 +1,43 @@
 import Layout from '../common/Layout';
+import { useRef, useState } from 'react';
 
 function Community() {
+	const input = useRef(null);
+	const textarea = useRef(null);
+
+	const dummyPosts = [
+		{ title: 'Hello2', content: 'Here comes description in detail.' },
+		{ title: 'Hello1', content: 'Here comes description in detail.' },
+	];
+
+	const [posts, setPosts] = useState(dummyPosts);
+
 	return (
 		<Layout name={'Community'}>
-			<p>커뮤니티 컨텐츠가 들어올자리</p>
+			<div className='inputBox'>
+				<input type='text' placeholder='제목을 입력하세요' ref={input} />
+				<br />
+				<textarea
+					ref={textarea}
+					cols='30'
+					rows='10'
+					placeholder='본문을 입력하세요'></textarea>
+				<br />
+
+				<button>cancel</button>
+				<button>create</button>
+			</div>
+
+			<div className='showBox'>
+				{posts.map((post, idx) => {
+					return (
+						<article key={idx}>
+							<h2>{post.title}</h2>
+							<p>{post.content}</p>
+						</article>
+					);
+				})}
+			</div>
 		</Layout>
 	);
 }
