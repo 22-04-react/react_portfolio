c7a516344c355118608e9cb82b910f63dcbd1bca
tubi55 <tubi55@nate.com>
2022-05-11 10:43:32 +0900

    async await를 이용해서 동기적으로(순차적으로) 데이터 요청 처리

--- src/components/sub/Flickr.js
+++ src/components/sub/Flickr.js
@@ -1,25 +1,37 @@
 import Layout from '../common/Layout';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 
 function Flickr() {
+	const frame = useRef(null);
 	const [items, setItems] = useState([]);
 
-	useEffect(() => {
+	//wrapping함수에 async키워드를 추가하고
+	const getFlickr = async () => {
 		const key = '4612601b324a2fe5a1f5f7402bf8d87a';
 		const method_interest = 'flickr.interestingness.getList';
-		const num = 20;
+		const num = 500;
 		const url = `https://www.flickr.com/services/rest/?method=${method_interest}&per_page=${num}&api_key=${key}&nojsoncallback=1&format=json`;
 
-		axios.get(url).then((json) => {
+		//순차적으로 동작이 되야 되는 함수에 await 키워드 추가
+		await axios.get(url).then((json) => {
 			console.log(json.data.photos.photo);
 			setItems(json.data.photos.photo);
 		});
+
+		//다음 코드들은 위의 await구문이 붙어있는 코드의 동작이 완료된 직후에 동기적으로 실행됨
+		//결과적으로 모든 데이터가 flickr로 부터 다 불러와져야지 갤러리 출력됨
+		console.log('flickr데이터 호출끝!! 화면 출력시작');
+		frame.current.classList.add('on');
+	};
+
+	useEffect(() => {
+		getFlickr();
 	}, []);
 
 	return (
 		<Layout name={'Flickr'}>
-			<div className='frame on'>
+			<div className='frame' ref={frame}>
 				{items.map((item, idx) => {
 					return (
 						<article key={idx}>
