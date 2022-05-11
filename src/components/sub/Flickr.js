b708571ee3ddf4a77de47f8eef9d77d9e680fffd
tubi55 <tubi55@nate.com>
2022-05-11 10:21:13 +0900

    로딩 이미지 추가

--- src/components/sub/Flickr.js
+++ src/components/sub/Flickr.js
@@ -1,8 +1,42 @@
 import Layout from '../common/Layout';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Flickr() {
-	return <Layout name={'Flickr'}>flickr</Layout>;
+	const [items, setItems] = useState([]);
+
+	useEffect(() => {
+		const key = '4612601b324a2fe5a1f5f7402bf8d87a';
+		const method_interest = 'flickr.interestingness.getList';
+		const num = 20;
+		const url = `https://www.flickr.com/services/rest/?method=${method_interest}&per_page=${num}&api_key=${key}&nojsoncallback=1&format=json`;
+
+		axios.get(url).then((json) => {
+			console.log(json.data.photos.photo);
+			setItems(json.data.photos.photo);
+		});
+	}, []);
+
+	return (
+		<Layout name={'Flickr'}>
+			<div className='frame on'>
+				{items.map((item, idx) => {
+					return (
+						<article key={idx}>
+							<div className='inner'>
+								<div className='pic'>
+									<img
+										src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`}
+									/>
+								</div>
+								<h2>{item.title}</h2>
+							</div>
+						</article>
+					);
+				})}
+			</div>
+		</Layout>
+	);
 }
 
 export default Flickr;
