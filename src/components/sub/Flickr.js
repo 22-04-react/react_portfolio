09cc63b3dbd81feefb5939b853745161ebaeebbb
tubi55 <tubi55@nate.com>
2022-05-11 11:26:50 +0900

    masonry 적용

--- src/components/sub/Flickr.js
+++ src/components/sub/Flickr.js
@@ -1,20 +1,22 @@
 import Layout from '../common/Layout';
 import { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
+import Masonry from 'react-masonry-component';
 
 function Flickr() {
 	const frame = useRef(null);
 	const [items, setItems] = useState([]);
+	const masonryOptions = {
+		transitionDuration: '0.5s',
+	};
 
 	const key = '4612601b324a2fe5a1f5f7402bf8d87a';
 	const num = 50;
 	const interest_url = `https://www.flickr.com/services/rest/?method=flickr.interestingness.getList&per_page=${num}&api_key=${key}&nojsoncallback=1&format=json`;
 	const search_url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&per_page=${num}&api_key=${key}&nojsoncallback=1&format=json&tags=ocean`;
 
-	//getFlickr함수에 원하는 주소값을 전달할 수 있도록 url이라는 파라미터 추가
 	const getFlickr = async (url) => {
 		await axios.get(url).then((json) => {
-			console.log(json.data.photos.photo);
 			setItems(json.data.photos.photo);
 		});
 
@@ -22,13 +24,11 @@ function Flickr() {
 	};
 
 	useEffect(() => {
-		//처음 컴포넌트 로딩시 interest 인수로 해당 갤러리 화면 출력
 		getFlickr(interest_url);
 	}, []);
 
 	return (
 		<Layout name={'Flickr'}>
-			{/* 버튼 클릭시 frame에 on을 지워서 기존 갤러리 사라지게 하고 interest갤러리 호출 */}
 			<button
 				onClick={() => {
 					frame.current.classList.remove('on');
@@ -37,7 +37,6 @@ function Flickr() {
 				interest gallery
 			</button>
 
-			{/* 버튼 클릭시 frame에 on을 지워서 기존 갤러리 사라지게 하고 search갤러리 호출 */}
 			<button
 				onClick={() => {
 					frame.current.classList.remove('on');
@@ -47,20 +46,22 @@ function Flickr() {
 			</button>
 
 			<div className='frame' ref={frame}>
-				{items.map((item, idx) => {
-					return (
-						<article key={idx}>
-							<div className='inner'>
-								<div className='pic'>
-									<img
-										src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`}
-									/>
+				<Masonry elementType={'div'} options={masonryOptions}>
+					{items.map((item, idx) => {
+						return (
+							<article key={idx}>
+								<div className='inner'>
+									<div className='pic'>
+										<img
+											src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`}
+										/>
+									</div>
+									<h2>{item.title}</h2>
 								</div>
-								<h2>{item.title}</h2>
-							</div>
-						</article>
-					);
-				})}
+							</article>
+						);
+					})}
+				</Masonry>
 			</div>
 		</Layout>
 	);
