0a10befd3fcb71ad1779a3f82c37a8c72b325a92
tubi55 <tubi55@nate.com>
2022-05-12 16:51:14 +0900

    swiper적용

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 function Visual() {
-	return <figure className='myScroll on'></figure>;
+	return (
+		<figure className='myScroll on'>
+			<Swiper
+				spaceBetween={50}
+				loop={true}
+				slidesPerView={3}
+				pagination={{
+					clickable: true,
+				}}
+				navigation={true}
+				modules={[Pagination, Navigation]}>
+				<SwiperSlide>1</SwiperSlide>
+				<SwiperSlide>2</SwiperSlide>
+				<SwiperSlide>3</SwiperSlide>
+				<SwiperSlide>4</SwiperSlide>
+				<SwiperSlide>5</SwiperSlide>
+			</Swiper>
+		</figure>
+	);
 }
 
 export default Visual;
