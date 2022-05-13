7120db5c36b55cae764d2ad94c8c463f36a30a7c
tubi55 <tubi55@nate.com>
2022-05-13 10:46:03 +0900

    youtube 비동기 데이터 store저장

--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -2,27 +2,43 @@ import Layout from '../common/Layout';
 import { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import Popup from '../common/Popup';
+import { useSelector, useDispatch } from 'react-redux';
+import { setYoutube } from '../../redux/action';
 
 function Youtube() {
+	//store에서 youtubeReducer 데이터를 가져옴 (빈배열)
+	const vidData = useSelector((store) => store.youtubeReducer.youtube);
+	console.log(vidData);
+
+	//액션객체를 reducer로 전달해주는 함수 할당
+	const dispatch = useDispatch();
 	const pop = useRef(null);
-	const [vids, setVids] = useState([]);
 	const [index, setIndex] = useState(0);
 
-	useEffect(() => {
+	const fetchYoutube = async () => {
 		const key = 'AIzaSyBZFBuapkASPcRBXB2-d_ak5-ecCpVicI4';
 		const playlistId = 'PLHtvRFLN5v-UVVpNfWqtgZ6YPs9ZJMWRK';
 		const num = 5;
 		const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playlistId}&maxResults=${num}`;
 
-		axios.get(url).then((json) => {
-			setVids(json.data.items);
+		await axios.get(url).then((json) => {
+			//axios로 받아온 데이터를 setYoutube로 함수로 action객체를 반환하고
+			//반환된 action객체를 dispatch로 reducer에 전달
+			dispatch(setYoutube(json.data.items));
 		});
+	};
+
+	useEffect(() => {
+		//해당 함수가 실행되면 axios가 받아진 데이타가 reducer로 전달되서
+		//전역 store에 담기고
+		//다시 store를 통해서 화면 랜더링
+		fetchYoutube();
 	}, []);
 
 	return (
 		<>
 			<Layout name={'Youtube'}>
-				{vids.map((vid, idx) => {
+				{vidData.map((vid, idx) => {
 					const tit = vid.snippet.title;
 					const desc = vid.snippet.description;
 					const date = vid.snippet.publishedAt;
@@ -46,10 +62,10 @@ function Youtube() {
 			</Layout>
 
 			<Popup ref={pop}>
-				{vids.length !== 0 ? (
+				{vidData.length !== 0 ? (
 					<>
 						<iframe
-							src={`https://www.youtube.com/embed/${vids[index].snippet.resourceId.videoId}`}
+							src={`https://www.youtube.com/embed/${vidData[index].snippet.resourceId.videoId}`}
 							frameBorder='0'></iframe>
 						<span className='close' onClick={() => pop.current.close()}>
 							close
