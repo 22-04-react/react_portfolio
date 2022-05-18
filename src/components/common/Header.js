6f84e7fb359468944c15eaa9627e921db6251bec
tubi55 <tubi55@nate.com>
2022-05-18 12:17:45 +0900

    브라우저 리사이즈시 데스크톱 사이즈로 넘어갈시 모바일 패널 자동 닫기

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -8,18 +8,45 @@ import SvgLine from '../../class/svg_line';
 function Header(props) {
 	const menu = useRef(null);
 	const pathTop = useRef(null);
+	const pathBottom = useRef(null);
 	const active = { color: 'aqua' };
 	const [toggle, setToggle] = useState(false);
+
+	const closeMobile = () => {
+		setToggle(false);
+		const wid = window.innerWidth;
+		if (wid > 1179) {
+			menu.current.close();
+
+			SvgLine.put(pathTop.current, {
+				start: '0%',
+				end: '15%',
+			});
+
+			SvgLine.put(pathBottom.current, {
+				start: '0%',
+				end: '13%',
+			});
+		}
+	};
 
 	useEffect(() => {
 		toggle ? menu.current.open() : menu.current.close();
 	}, [toggle]);
 
 	useEffect(() => {
+		window.addEventListener('resize', closeMobile);
 		SvgLine.put(pathTop.current, {
+			start: '0%',
+			end: '15%',
+		});
+
+		SvgLine.put(pathBottom.current, {
 			start: '0%',
 			end: '13%',
 		});
+
+		return () => window.removeEventListener('resize', closeMobile);
 	}, []);
 
 	return (
@@ -72,8 +99,15 @@ function Header(props) {
 								setToggle(!toggle);
 								SvgLine.animate(pathTop.current, {
 									start: '0%',
+									end: '14%',
+									duration: 0.5,
+								});
+
+								SvgLine.animate(pathBottom.current, {
+									start: '0%',
 									end: '13%',
 									duration: 0.5,
+									delay: 0.2,
 								});
 								if (toggle) {
 								} else {
@@ -82,14 +116,25 @@ function Header(props) {
 										end: '100%',
 										duration: 0.5,
 									});
+									SvgLine.animate(pathBottom.current, {
+										start: '70%',
+										end: '100%',
+										duration: 0.5,
+										delay: 0.2,
+									});
 								}
 							}}>
 							<svg width='80' height='80' viewBox='0 0 80 80' fill='none'>
 								<path
 									d='M16.5 1H55.5C61 3 72 9.8 72 21C72 35 65.5 56 36 56C6.5 56 0.5 36.5 0.5 21C0.5 8.6 11.1667 5.16667 16.5 5L55.5 37'
 									transform='translate(5 20)'
-									id='pathTop'
 									ref={pathTop}
+								/>
+
+								<path
+									d='M16.5 56L55.5 56C61 54 72 47.2 72 36C72 22 65.5 0.999994 36 0.999997C6.5 0.999999 0.499997 20.5 0.499998 36C0.499999 48.4 11.1667 51.8333 16.5 52L55.5 20M16.5 56L55.5 56'
+									transform='translate(5 5)'
+									ref={pathBottom}
 								/>
 							</svg>
 						</span>
