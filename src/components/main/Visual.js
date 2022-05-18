55d3801a7d03c2afd96dc164c43f5dfc30515cea
tubi55 <tubi55@nate.com>
2022-05-18 09:25:23 +0900

    비주얼 영역에서 마우스 포인터 모양 변경

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -1,11 +1,24 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Navigation } from 'swiper';
+import { useRef, useEffect } from 'react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 const path = process.env.PUBLIC_URL;
 
 function Visual() {
+	const cursor = useRef(null);
+
+	const mouseMove = (e) => {
+		cursor.current.style.left = e.clientX + 'px';
+		cursor.current.style.top = e.clientY + 'px';
+	};
+
+	useEffect(() => {
+		window.addEventListener('mousemove', mouseMove);
+		return () => window.removeEventListener('mousemove', mouseMove);
+	}, []);
+
 	return (
 		<figure className='myScroll on'>
 			<Swiper
@@ -39,6 +52,8 @@ function Visual() {
 					<video src={`${path}/img/vid5.mp4`} loop autoPlay muted></video>
 				</SwiperSlide>
 			</Swiper>
+
+			<div className='cursor' ref={cursor}></div>
 		</figure>
 	);
 }
